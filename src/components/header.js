b2840d09bf302f22a8c31b682086bb1cceb1fba9
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-08-04 00:13:35 +0100

    adding state to header component

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,54 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Header extends Component {
-    render () {
-        return (
-            <header>
-                <div className="logo">Logo</div>
-                <input/>
-            </header>
-        )
+
+    state = {
+        title: 'The Application Keywords are: ',
+        keywords: '',
+        count: 0
     }
+
+
+  hello = () => {
+    console.log("hello mo mo");
+  };
+
+
+  inputChange = (event) => {
+      this.setState({
+          keywords: event.target.value
+      })
+  }
+
+  addOne = () => {
+      this.setState((state,props)=> ({
+          count: state.count + 1
+      }))
+  }
+
+
+  render() {
+    return (
+      <header>
+        <div className="logo" onClick={this.hello}>
+          Logo
+        </div>
+        <input onChange={ (event) => this.inputChange(event) } />
+        <div>
+            { this.state.title }
+        </div>
+        <div>
+            { this.state.keywords }
+        </div>
+
+        <br/>
+        <div>
+            { this.state.count }
+        </div>
+        <button onClick={ ()=> this.addOne() }>Add One</button>
+      </header>
+    );
+  }
 }
 
 export default Header;
