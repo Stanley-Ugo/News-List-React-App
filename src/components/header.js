68b3e6b965183dba5a6e61a0fc3b94471b522a4e
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-08-04 00:43:44 +0100

    making NewsList a functional component

--- src/components/header.js
+++ src/components/header.js
@@ -3,27 +3,14 @@ import React, { Component } from "react";
 class Header extends Component {
 
     state = {
-        title: 'The Application Keywords are: ',
-        keywords: '',
-        count: 0
+        keywords: ''
     }
-
-
-  hello = () => {
-    console.log("hello mo mo");
-  };
 
 
   inputChange = (event) => {
       this.setState({
           keywords: event.target.value
       })
-  }
-
-  addOne = () => {
-      this.setState((state,props)=> ({
-          count: state.count + 1
-      }))
   }
 
 
@@ -34,18 +21,6 @@ class Header extends Component {
           Logo
         </div>
         <input onChange={ (event) => this.inputChange(event) } />
-        <div>
-            { this.state.title }
-        </div>
-        <div>
-            { this.state.keywords }
-        </div>
-
-        <br/>
-        <div>
-            { this.state.count }
-        </div>
-        <button onClick={ ()=> this.addOne() }>Add One</button>
       </header>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import Header from './components/header';
-import styles from './styles/styles.css';
 
-const App = () => {
-  return (
-    <>
-      <Header/>
-    </>
-  );
-};
+import Header from "./components/header";
+
+import "./styles/styles.css";
+
+import JSON from "./db.json";
+
+class App extends Component {
+  constructor(){
+    super()
+
+    this.state = {
+      news: JSON
+    }
+  }
+  render() {
+    return (
+      <>
+        <Header />
+      </>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
